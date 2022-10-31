f8a679dee46747347923a68af0e72252a86885b6
YoannL97 <yoann-lystig.77@hotmail.com>
2022-10-31 10:35:33 +0100

    31/10/22

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,10 +1,67 @@
 
-let nbrItems = localStorage.length
-for (let i = 0; i < nbrItems ; i++ ) {
-    let items = localStorage.getItem(localStorage.key(i))
-    let itemsobjet = JSON.parse(items)
-    
+function displayCartLine(toto) {
+    console.log(toto)
+    let section = document.getElementById("cart__items")
+    let article = document.createElement("article")
+    article.className = "cart__item"
+    section.appendChild(article)
 
+    let divImage = document.createElement("div")
+    divImage.className = "cart__item__img"
+    article.appendChild(divImage)
+
+
+    let imgImage = document.createElement("img")
+    divImage.appendChild(imgImage)
+    imgImage.src = toto.imageUrl
+
+
+    let itemsContentClass = document.createElement("div")
+    itemsContentClass.className = "cart__item__content"
+    article.appendChild(itemsContentClass)
+
+    let itemsContentDescription = document.createElement("div")
+    itemsContentDescription.className = "cart__item__content__description"
+    itemsContentClass.appendChild(itemsContentDescription)
+
+    let titreH2 = document.createElement("h2")
+    itemsContentDescription.appendChild(titreH2)
+
+    let colorP = document.createElement("p")
+    colorP.textContent = toto.couleur
+    itemsContentDescription.appendChild(colorP)
+
+    let priceP = document.createElement("p")
+    itemsContentDescription.appendChild(priceP)
+
+    let contentSetting = document.createElement("div")
+    contentSetting.className = "cart__item__content__settings"
+    article.appendChild(contentSetting)
+
+    let contentQuantity = document.createElement("div")
+    contentQuantity.className = "cart__item__content__settings__quantity"
+    contentSetting.appendChild(contentQuantity)
+
+    let quantityP = document.createElement("p")
+    contentQuantity.appendChild(quantityP)
+}
+
+
+
+let itemsList = JSON.parse(localStorage.getItem('panier'))
+
+for (let produit of itemsList) {
+//colorP.textContent = produit.couleur
+console.log(produit.id)
+//displayCartLine(produit)
+fetch (`http://localhost:3000/api/products/${produit.id}`)
+.then ((response) => response.json())
+.then ((data) => {
+    data['couleur'] = produit.couleur
+    data['qty'] = produit.quantité
+    displayCartLine(data)})
+.catch((error) => {
+    console.log(error)})
 
 }
 
@@ -12,20 +69,7 @@ for (let i = 0; i < nbrItems ; i++ ) {
 
 
 
-let section = document.getElementById("cart__items")
-let article = document.createElement("article")
-article.className = "cart__item"
-section.appendChild(article)
-
-let divImage = document.createElement("div")
-divImage.className = "cart__item__img"
-article.appendChild(divImage)
-
-let imgImage = document.createElement("img")
-divImage.appendChild(imgImage)
 
 
-let itemsContentClass = document.createElement("div")
-itemsContentClass.className = "cart__item__content"
-article.appendChild(itemsContentClass)
+
 
--- front/js/product.js
+++ front/js/product.js
@@ -1,12 +1,13 @@
 const idNumber = window.location.search
 const urlparam = new URLSearchParams(idNumber)
 const id = urlparam.get("id")
-let imgUrl;
 
 
 fetch (`http://localhost:3000/api/products/${id}`)
 .then ((response) => response.json())
 .then ((data) => addobject (data))
+.catch((error) => {
+    console.log(error)})
 
 function addobject (canape) {
 
@@ -18,8 +19,6 @@ let imgClass = document.getElementsByClassName("item__img")
 
 imgClass[0].appendChild(image)
 image.alt = canape.altTxt
-
-imgUrl = image
 
 //  titre
 
@@ -47,7 +46,6 @@ for (let color_id in canape.colors) {
     select.appendChild(myOption)
 
 
-let imgUrl = image
 }
 }
 
@@ -57,47 +55,93 @@ let imgUrl = image
 
 
 
-function boutonEvent (panier) {
+function boutonEvent () {
 
 let bouton = document.querySelector("#addToCart")
-bouton.addEventListener ("click", (cana) => {
+bouton.addEventListener ("click", (e) => {
 
 let color = document.getElementById("colors").value
 let quantite = document.getElementById("quantity").value
-let price = document.getElementById("price").textContent
-let titre = document.getElementById("title").textContent
-
-
-
-let panier = {
-    couleur: color,
-    quantité: quantite,
-    prix : price,
-    titre : titre,
-    image : imgUrl.src,
-    
-
-    
-}
 
 if (color === "" || color == "null" | quantite == "0" | quantite == "null")
     alert ("veuillez choisir une couleur et une quantité")
 
-    
-localStorage.setItem("panier", JSON.stringify(panier)) 
 
+panier = {
+    id: id,
+    couleur: color,
+    quantite: quantite,  
+}
+
+
+
+
+
+localStorage.setItem( 'panier', JSON.stringify(panier)) 
+
+if (localStorage.getItem('panier') == "null") {
+    localStorage.setItem('panier', '[]' )
+}
+
+let cart = JSON.parse(localStorage.getItem('panier'))
+cart.push(panier)
+
+localStorage.setItem('panier', JSON.stringify(cart))
 
 
 })
 }
 
+
+
+/*let cart = JSON.parse(localStorage.getItem('panier'))
+cart = Array()
+cart.push(panier)    
+if (panier.id == id )
+*/
+
+/*function getPanier () {
+
+    boutonEvent()
+    let cart = Array()
+    cart = JSON.parse(localStorage.getItem('panier'))
+console.log(cart);
+}
+
+function addPanier () {
+
+    let yoyo = getPanier();
+    console.log(yoyo);
+}
+getPanier()
+addPanier()
+
+
+/*
+si (le panier existe) {
+    let panier = {
+        id: id,
+        couleur: color,
+        quantité: quantite,  
+    }
+    si (l'id et la couleur existent) {
+        modifier la quantité
+    }
+    sinon {
+        ajouter l'article
+    }
+    cart.push(panier)    
+}
+sinon {
+    créer le panier
+}
+*/
+
+
+//let cart = JSON.parse(localStorage.getItem('panier'))
+//cart = Array()
+//cart.push(panier) 
+//localStorage.setItem( 'panier', JSON.stringify(panier)) 
+
+
 boutonEvent()
-
-
-
-
-
-
-
-
-
